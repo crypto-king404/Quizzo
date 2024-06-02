2ff1c6ba2ef2a01a8c6cf7273ae43b6521b9f7dd
Sohan Hajra <68421638+crypto-king404@users.noreply.github.com>
2024-06-02 23:25:36 +0400

    Update dummy_quiz_ui.py

--- tasks/Streamlit UI/dummy_quiz_ui.py
+++ tasks/Streamlit UI/dummy_quiz_ui.py
@@ -17,7 +17,7 @@ To Build a Quiz Builder with Streamlit and LangChain
 #     # Configuration for EmbeddingClient
 #     embed_config = {
 #         "model_name": "textembedding-gecko@003",
-#         "project": "quizzify-423703",
+#         "project": "PROJECT-123",
 #         "location": "us-central1"
 #     }
     
@@ -31,37 +31,36 @@ To Build a Quiz Builder with Streamlit and LangChain
 
 #         embed_config = {
 #         "model_name": "textembedding-gecko@003",
-#         "project": "quizzify-423703",
-#         "key_file_path": "C:/Users/sohan/OneDrive/Documents/quizify/mission-quizify/authentication.json",
+#         "project": "PROJECT-123",
+#         "key_file_path": "C:/path/to/the/json/authentication.json",
 #         "location": "us-central1"
 #         }
 
-#         # 2) Initalize the EmbeddingClient from Task 4 with embed config
+
 #         embed_client = EmbeddingClient(**embed_config)
 
-#         # 3) Initialize the ChromaCollectionCreator from Task 5
-#         ####### YOUR CODE HERE #######
+# 
 #         chroma_creator = ChromaCollectionCreator(processor, embed_client)
 
 #         with st.form("Load Data to Chroma"):
 #             st.subheader("Quiz Builder")
 #             st.write("Select PDFs for Ingestion, the topic for the quiz, and click Generate!")
             
-#             ####### YOUR CODE HERE #######
-#             # 4) Use streamlit widgets to capture the user's input
+#        
+#             #Use streamlit widgets to capture the user's input
 #             topic = st.text_input("Enter the quiz topic:")
 
-#             # 4) for the quiz topic and the desired number of questions
-#             ####### YOUR CODE HERE #######
+#             # for the quiz topic and the desired number of questions
+#        
 #             num_questions = st.slider("Select the number of questions", min_value=1, max_value= 10, value = 5)
 
 #             document = None
             
 #             submitted = st.form_submit_button("Generate a Quiz!")
 #             if submitted:
-#                 ####### YOUR CODE HERE #######
+#          
 #                 # 5) Use the create_chroma_collection() method to create a Chroma collection from the processed documents
-#                 ####### YOUR CODE HERE #######
+#           
 
 #                 chroma_collection = chroma_creator.create_chroma_collection()
                     
