b059b5ef7d019955624783f6f1e4101bc7e78aac
Sohan Hajra <68421638+crypto-king404@users.noreply.github.com>
2024-06-04 08:49:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-__import__(‘pysqlite3’)
+__import__('pysqlite')
 import sys
 
-sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import os
