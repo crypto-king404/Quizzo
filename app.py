32fc8928468294f7735cd07bec9bf8f9911c7f8c
Sohan Hajra <68421638+crypto-king404@users.noreply.github.com>
2024-06-05 17:55:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ if __name__ == "__main__":
     
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "quizzify-423703",
-        "key_file_path": "authentication.json",
+        "project": "quizzo-425513",
         "location": "us-central1"
     }
     
